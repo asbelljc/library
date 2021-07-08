f304ff1d91eb681f4d58d4c1f9275ea8fd47e5c7
asbelljc <asbelljc@gmail.com>
2021-07-08 16:58:20 -0400

    test

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use("/", () => console.log("testmessage"));
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
