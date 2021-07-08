29c4511d5a599790e3350adfa66d5aa072f3473c
asbelljc <asbelljc@gmail.com>
2021-07-08 16:54:09 -0400

    Trying deployment again...

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("testMessage");
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
