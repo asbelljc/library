74f6db2a9c89d90d5a73650eb8371137704d2d42
aflynn93 <43562703+aflynn93@users.noreply.github.com>
2021-07-17 12:02:12 -0500

    Move checkbox out of the toggle label. Set checkbox initial state to match toggle state.

--- script.js
+++ script.js
@@ -30,6 +30,33 @@ toggleSwitches.forEach(item => {
   });
 });
 
+
+// TODO - We can do something like this to set the checkbox to whatever the toggle is set to,
+// but I'm not sure if it is needed. I'm wondering if the checkboxes themselves are actually needed.
+// Can we just set the initial toggle state to match the database, and then update the database based
+// on the toggle?
+// One more thing to consider - when new cards get deleted from the UI, they should also be deleted from
+// the database. But for the array in memory, we can probably just leave them in the array with a flag
+// that incidates deletion. This way, our indexing is not messed up and we don't slow things down by
+// creating a new array after each delete.
+// Also, putting a reminder here to talk about "soft" deletes versus actual deletes.
+let toggles = document.querySelectorAll('label[id="readToggle"]');
+
+for (let index=0; index < toggles.length; ++index) {
+  if (toggles[index].classList.contains("toggled")) {
+    console.log("toggled class is present, so setting myCheckbox.checked to true");
+    
+    let myCheckbox = document.querySelector('input[id="mycheckbox"]');
+    myCheckbox.checked = "true";
+  } else {
+    console.log("toggled class is not present, so setting myCheckbox.checked to false");
+    
+    let myCheckbox = document.querySelector('input[id="mycheckbox"]');
+    myCheckbox.checked = "false";
+  }
+}
+
+
 const addButton = document.getElementById("add-btn");
 
 addButton.addEventListener("click", () => {
@@ -44,6 +71,18 @@ readToggle.addEventListener("click", () => {
 });
 
 const deleteButton = document.getElementsByClassName("delete-btn")[0];
+
+const readToggle2 = document.getElementsByClassName("read-toggle")[1];
+
+readToggle2.addEventListener("click", () => {
+  readToggle2.classList.toggle("toggled");
+  readToggle2.firstElementChild.classList.toggle("toggled");
+
+  let myCheckbox = document.querySelector('input[id="mycheckbox"]');
+  console.log("myCheckbox.checked is " + myCheckbox.checked + ", setting to " + !(myCheckbox.checked));
+  myCheckbox.checked = !(myCheckbox.checked);
+  // TODO - This is where the API call will happen for marking a book read/unread
+});
 
 function toggleDelete() {
   deleteButton.classList.toggle("toggled");
